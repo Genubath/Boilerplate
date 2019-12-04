bc75d4e74a244f8a88d73ee0cb6ba7ae07086498
Logan Thirion <genubath15@gmail.com>
2019-12-04 13:53:10 -0600

    Routing

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Login from "./Pages/Login/Login";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About"
+import Dashboard from "./Pages/Dashboard/Dashboard"
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/dashboard" component={Dashboard} />
+      {/*<Route path="/login" exact component={Login} /> */}
+    </Switch>
+  );
+}
