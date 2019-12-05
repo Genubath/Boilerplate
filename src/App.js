a416a6946b46f36d61a6244980c39388276afd56
Logan Thirion <genubath15@gmail.com>
2019-12-05 14:33:22 -0600

    changing css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 
 export default function App(props) {
   return (
-    <div className="App container">
-      <Navbar collapseOnSelect>
-        <Navbar.Brand>
-          <Link to="/">Home</Link>
-        </Navbar.Brand>
+    <Container fluid bg="dark" variant="dark">
+      <Navbar collapseOnSelect bg="dark" variant="dark">
+        <Navbar.Brand href="/">Home</Navbar.Brand>
         <Navbar.Toggle />
       </Navbar>
       <Routes />
-    </div>
+    </Container>
   );
 }
