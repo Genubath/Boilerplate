eb5c05727b69d52ddfceb70ce50284daa6960ea3
Genubath <genubath15@gmail.com>
2019-12-04 18:06:36 -0600

    add login page

--- src/Routes.js
+++ src/Routes.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Home from "./Pages/Home/Home";
-import About from "./Pages/About/About"
-import Dashboard from "./Pages/Dashboard/Dashboard"
+import About from "./Pages/About/About";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 
 export default function Routes() {
   return (
@@ -11,7 +11,7 @@ export default function Routes() {
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/dashboard" component={Dashboard} />
-      {/*<Route path="/login" exact component={Login} /> */}
+      <Route path="/login" exact component={Login} />
     </Switch>
   );
 }
