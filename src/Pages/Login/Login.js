24928746a5a62c89bfd9a23be3133f2c824ca3ec
Genubath <genubath15@gmail.com>
2019-12-05 21:53:33 -0600

    Dummy Logging in

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button, Jumbotron } from "react-bootstrap";
+// import { Auth } from "aws-amplify";
 import "./Login.css";
 
 export default function Login(props) {
@@ -10,8 +11,15 @@ export default function Login(props) {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+
+    try {
+      // await Auth.signIn(email, password);
+      props.userHasAuthenticated(true);
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   return (
