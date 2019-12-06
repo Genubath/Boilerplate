998b4b03f22d4e4e1650aacb56136a73c72a5b2e
Genubath <genubath15@gmail.com>
2019-12-05 21:41:13 -0600

    404 page

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <h1>404!</h1>
+      <h3>Sorry, page not found!</h3>
+    </div>
+  );
+}
