cda536f2b43a65b4da7b9b607d4c7a0d91d9037b
Genubath <genubath15@gmail.com>
2019-12-08 10:26:28 -0600

    formatting

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Routes from "./Routes";
 
 function App(props) {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+
   function handleLogout() {
     userHasAuthenticated(false);
     props.history.push("/login");
@@ -21,7 +22,9 @@ function App(props) {
   return (
     <div className="App">
       <Navbar collapseOnSelect bg="dark" variant="dark">
-        <Navbar.Brand to="/" as={Link}>Home</Navbar.Brand>
+        <Navbar.Brand to="/" as={Link}>
+          Home
+        </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse id="basic-navbar-nav">
           <Form inline>
@@ -32,13 +35,19 @@ function App(props) {
             <Nav.Link to="/about" as={Link}>
               Why boilerplate?
             </Nav.Link>
-            <Nav.Link to="/dashboard" as={Link}>Dashboard</Nav.Link>
+            <Nav.Link to="/dashboard" as={Link}>
+              Dashboard
+            </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item to="#action/3.1" as={Link}>Action</NavDropdown.Item>
+              <NavDropdown.Item to="#action/3.1" as={Link}>
+                Action
+              </NavDropdown.Item>
               <NavDropdown.Item to="#action/3.2" as={Link}>
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item to="#action/3.3" as={Link}>Something</NavDropdown.Item>
+              <NavDropdown.Item to="#action/3.3" as={Link}>
+                Something
+              </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item to="#action/3.4" as={Link}>
                 Separated link
@@ -50,8 +59,12 @@ function App(props) {
               <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
             ) : (
               <>
-                <Nav.Link to="/signup" as={Link}>Signup</Nav.Link>
-                <Nav.Link to="/login" as={Link}>Login</Nav.Link>
+                <Nav.Link to="/signup" as={Link}>
+                  Signup
+                </Nav.Link>
+                <Nav.Link to="/login" as={Link}>
+                  Login
+                </Nav.Link>
               </>
             )}
           </Nav>
