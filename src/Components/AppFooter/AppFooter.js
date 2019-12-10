9480aeecf29d08413e18c106b457c49d41ed8209
Genubath <genubath15@gmail.com>
2019-12-09 20:54:29 -0600

    add css

--- src/Components/AppFooter/AppFooter.js
+++ src/Components/AppFooter/AppFooter.js
@@ -4,14 +4,14 @@ import "./AppFooter.css";
 
 export default function AppFooter() {
   return (
-    <footer>
-      <div>
-        <a href="https://coreui.io">CoreUI</a>
-        <span>&copy; 2018 creativeLabs.</span>
+    <footer className="footer">
+      <div className="startContainer">
+        <a href="https://react-bootstrap.github.io/">Boilerplate </a>
+        <span>&copy; 2018 Me.</span>
       </div>
-      <div>
+      <div className="endContainer">
         <span>Powered by</span>
-        <a href="https://coreui.io">CoreUI</a>
+        <a href="https://react-bootstrap.github.io/"> React Bootstrap</a>
       </div>
     </footer>
   );
